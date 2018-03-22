9fc62c11a9f84195e04a8cca78a28802ae0159b5
vikashraj144 <vikashraj144@gmail.com>
2018-03-22 18:33:26 +0530

    connection added

---
+++ dbconnection.js
@@ -0,0 +1,9 @@
+var mysql=require('mysql');
+ var connection=mysql.createPool({
+ 
+host:'localhost',
+ user:'root',
+ password:'welcome',
+ database:'node-express-mysql'
+});
+ module.exports=connection;
