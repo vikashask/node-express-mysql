ce4b40113126718e5f95e79cfae57c3c41dddcf0
vikashraj144 <vikashraj144@gmail.com>
2018-04-16 11:38:17 +0530

    file update

--- dbconnection.js
+++ dbconnection.js
@@ -1,9 +1,8 @@
-var mysql=require('mysql');
- var connection=mysql.createPool({
- 
-host:'localhost',
- user:'root',
- password:'welcome',
- database:'node-express-mysql'
+var mysql = require('mysql');
+var connection = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'welcome',
+    database: 'node-express-mysql'
 });
- module.exports=connection;
+module.exports = connection;
---
+++ test/book.js
@@ -0,0 +1,38 @@
+//During the test the env variable is set to test
+process.env.NODE_ENV = 'test';
+
+// let mongoose = require("mongoose");
+let Book = require('../models/Task');
+
+//Require the dev-dependencies
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../app');
+let should = chai.should();
+
+chai.use(chaiHttp);
+
+//Our parent block
+describe('Books', () => {
+	beforeEach((done) => { //Before each test we empty the database
+		Book.remove({}, (err) => { 
+		   done();		   
+		});		
+	});
+ /*
+  * Test the /GET route
+  */
+  describe('/GET book', () => {
+	  it('it should GET all the books', (done) => {
+			chai.request(server)
+		    .get('/Tasks')
+		    .end((err, res) => {
+			  	res.should.have.status(200);
+			  	res.body.should.be.a('array');
+			  	res.body.length.should.be.eql(0);
+		      done();
+		    });
+	  });
+  });
+});
+  
