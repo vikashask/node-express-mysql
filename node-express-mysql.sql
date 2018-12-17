2f5ae1cc1bb6610e555902fca456f5ac3c33619b
vikash <vikashraj144@gmail.com>
2018-12-17 11:40:35 +0530

    msql added

--- node-express-mysql.sql
+++ node-express-mysql.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.6deb5
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Mar 23, 2018 at 10:33 AM
--- Server version: 5.7.21-0ubuntu0.17.10.1
--- PHP Version: 7.1.15-0ubuntu0.17.10.1
+-- Host: localhost
+-- Generation Time: Dec 17, 2018 at 07:09 AM
+-- Server version: 10.1.35-MariaDB
+-- PHP Version: 7.2.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -23,26 +25,54 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `meeting`
+--
+
+CREATE TABLE `meeting` (
+  `meeting_id` int(11) NOT NULL,
+  `time` datetime NOT NULL,
+  `meeting_type` tinyint(4) NOT NULL COMMENT '1=weekly,2=team meeting,3=follow up meeting,4= weekly sales meeting',
+  `status` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `meeting`
+--
+
+INSERT INTO `meeting` (`meeting_id`, `time`, `meeting_type`, `status`) VALUES
+(1, '2018-12-03 05:18:22', 1, 0);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `task`
 --
 
 CREATE TABLE `task` (
   `Id` int(50) NOT NULL,
   `Title` varchar(500) DEFAULT NULL,
-  `Status` varchar(100) DEFAULT NULL
+  `Status` varchar(100) DEFAULT NULL,
+  `Meeting_id` int(11) NOT NULL DEFAULT '1' COMMENT '1=weekly,2=team meeting,3=follow up meeting,4= weekly sales meeting'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `task`
 --
 
-INSERT INTO `task` (`Id`, `Title`, `Status`) VALUES
-(11, 'Email to manager34324343432432', 'pending'),
-(12, 'Email to ', 'pending');
+INSERT INTO `task` (`Id`, `Title`, `Status`, `Meeting_id`) VALUES
+(11, 'Email to manager34324343432432', 'pending', 0),
+(12, 'Email to ', 'pending', 0),
+(13, 'Email to ww', 'pending', 1);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `meeting`
+--
+ALTER TABLE `meeting`
+  ADD PRIMARY KEY (`meeting_id`);
 
 --
 -- Indexes for table `task`
@@ -55,10 +85,18 @@ ALTER TABLE `task`
 --
 
 --
+-- AUTO_INCREMENT for table `meeting`
+--
+ALTER TABLE `meeting`
+  MODIFY `meeting_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `task`
 --
 ALTER TABLE `task`
-  MODIFY `Id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `Id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
