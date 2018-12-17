393fa540400889c085a6f7212586d471e10304b7
vikash <vikashraj144@gmail.com>
2018-12-17 11:35:47 +0530

    removed view engin

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('view engine', 'jade');
 
 var debug = require('debug');
 app.set('port', process.env.PORT || 3000);
